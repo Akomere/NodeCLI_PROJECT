f00208298097d0d34371bc0c4105caa1ccfae777
Akomere <akomere.peter@gmail.com>
2022-01-14 18:16:31 +0000

    used reduce function to access the properties of the object using dot notationed string

--- index.js
+++ index.js
@@ -14,58 +14,102 @@ const fileNames = process.argv
 let rawdata = fs.readFileSync(fileNames[2]);
 let { actions } = JSON.parse(rawdata);
 
-
-
 const requestAction = "HTTPRequestAction"
 const printAction = "PrintAction"
 // let location = {}
-// const urlString = actions[1].options.url
+const urlString = actions[1].options.url
+console.log(urlString)
 const baseUrl = ""
 let requestObject = {}
+var eventVal = {}
 
 
-const getData = async (link) => {
 
+
+const index = (obj, i) => (
+    obj[i]
+)
+
+const urlHandler = () => {
+
+    let newString = ''
+    let idx = 0
+    let words = []
+
+    while (idx < urlString.length) {
+        if (urlString[idx] == '{' && urlString[idx + 1] == '{') {
+            let j = idx + 2
+            while (urlString[j] != '}') {
+                j++
+            }
+            console.log(urlString[j])
+
+            let param = urlString.slice(idx + 2, j)
+            let list = param.split('.')
+
+
+            words.push(list)
+            console.log(words)
+            idx = j + 2
+
+
+
+        }
+        else {
+            newString += urlString[idx]
+            idx++
+            console.log(newString)
+        }
+
+
+    }
+}
+
+const getData = async (link, name = "") => {
+
+    // if (eventVal.length !== 0) {
+    //     eval(name + " = eventVal.name");
+    // }
     try {
         const response = await axios.get(
             link
         );
-        location = response.data
-        // console.log(location) 
-        return response.data
+        eventVal[name] = response.data
+
+        let result = 'location.longitude'.split('.').reduce(index, eventVal)
+        console.log(result)
+
+
+        // console.log("event")
     } catch (err) {
         console.error(err);
     }
 }
 
 const actionHandler = async () => {
-    for (let i = 0; i <= actions.length - 1; i++)
+    for (let i = 0; i < 1; i++)
         switch (actions[i].type) {
             case requestAction:
                 const { options: { url } } = actions[i]
-                // console.log(url)
-                let response = await getData(url)
-                // console.log(response)
+                actionName = actions[i].name
+                getData(url, actionName)
+                console.log(eventVal)
 
-                eval(`${actions[i].name}` + " = response");
-                
                 // const {longitude} = location
                 // console.log(actions[i+1])
-                
-                const myUrl = actions[i].options.url
-                console.log(myUrl)
+
+                // const myUrl = actions[1].options.url
+                // // console.log(myUrl)
                 // try {
                 //     const response = await axios.get(
                 //         myUrl
                 //     );
                 //     result = response.data
                 //     console.log(result) 
-                     
+
                 // } catch (err) {
                 //     console.error(err);
                 // }
-
-
                 // const { longitude, latitude } = location
                 // console.log(location)
                 break;
@@ -76,6 +120,9 @@ const actionHandler = async () => {
             default:
                 break;
         }
+
+    console.log(eventVal)
+
 }
 
 
@@ -83,12 +130,12 @@ actionHandler()
 
 
 ///////////////////////////////////
-//split the url at the ? val
+//split the url at the '?' val to get base url
+//use brackets to store object strings and split them at '.'
+//
 
-// for (let idx = 0; idx < urlString.length; idx++){
-//     if (urlString[idx] == '{' && urlString[idx+1] == '{'  )
-//     console.log("json object")
 
-// }
+
+
 
 
